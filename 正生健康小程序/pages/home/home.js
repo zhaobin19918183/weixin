5b9d58c1869a2ee4cfd0b0789e1441affbb718df
zhaobin <4480779023@qq.com>
2018-09-05 09:59:19 +0800

    1

--- 正生健康小程序/pages/home/home.js
+++ 正生健康小程序/pages/home/home.js
@@ -1,4 +1,4 @@
-// pages/home/home.js
+// pages/home / home.js
 var wxCharts = require('../../utils/wxcharts.js');
 var app = getApp()
 var rate = 0;
@@ -6,28 +6,29 @@ var canvasWidth = 0;
 var canvasHeight = 0;
 var areaChart = null;
 var deviceHeight = false;
-var imgeUrlAni= ""
+var imgeUrlAni = ""
+var imgeUrlAni2 = ""
 // 获取数据的方法，具体怎么获取列表数据大家自行发挥
-var GetList = function(that) {
+var GetList = function (that) {
 
   that.setData({
     arrayData: [{
-        message: '分公司排行榜',
-        imgurl: "../imgs/img01_05.png",
-        id: 1
-      }, {
-        message: '服务中心排行榜',
-        imgurl: "../imgs/img01_07.png",
-        id: 2
-      }, {
-        message: '工作室排行榜',
-        imgurl: "../imgs/img01_09.png",
-        id: 3
-      }, {
-        message: '个人排行榜',
-        imgurl: "../imgs/img01_11.png",
-        id: 4
-      }
+      message: '分公司排行榜',
+      imgurl: "../imgs/img01_05.png",
+      id: 1
+    }, {
+      message: '服务中心排行榜',
+      imgurl: "../imgs/img01_07.png",
+      id: 2
+    }, {
+      message: '工作室排行榜',
+      imgurl: "../imgs/img01_09.png",
+      id: 3
+    }, {
+      message: '个人排行榜',
+      imgurl: "../imgs/img01_11.png",
+      id: 4
+    }
 
     ]
   })
@@ -39,7 +40,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    imgeUrlAni:"../imgs/gif1/1.jpg",
+    imgeUrlAni: "../imgs/gif1/1.jpg",
+    imgeUrlAni2: "../imgs/gif2/1.png",
     animationData: {},
     show_centent: false,
     if_show: false,
@@ -71,21 +73,21 @@ Page({
 
     // canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  touchHandler: function(e) {
+  touchHandler: function (e) {
     console.log(areaChart.getCurrentDataIndex(e));
     areaChart.showToolTip(e);
   },
 
-  durationChange: function(e) {
+  durationChange: function (e) {
     this.setData({
       duration: e.detail.value
     })
   },
-  btn: function() {
+  btn: function () {
 
 
   },
-  radioCheckedChange: function(e) {
+  radioCheckedChange: function (e) {
     console.log(e.detail.value)
     var that = this;
 
@@ -99,7 +101,7 @@ Page({
 
 
 
-  onLoad: function(e) {
+  onLoad: function (e) {
 
 
     var windowWidth = 320;
@@ -120,14 +122,14 @@ Page({
       animation: true,
       series: [{
         name: '总积分',
-        data: [1114000, 111200, 12000, ],
-        format: function(val) {
+        data: [1114000, 111200, 12000,],
+        format: function (val) {
           return '' + val.toFixed(2);
         }
       }],
       yAxis: {
         title: '',
-        format: function(val) {
+        format: function (val) {
           return val.toFixed(2);
         },
         min: 0,
@@ -212,18 +214,16 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
     console.log(123456789)
 
 
   },
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-    
+  // 动图实现方法
+  animationFunc:function()
+  {
     var animation = wx.createAnimation({
-      duration: 1000,
+      duration: 1,
       timingFunction: 'ease',
     })
     this.animation = animation
@@ -231,45 +231,56 @@ Page({
       animationData: animation.export()
     })
     var n = 0;
+    var k = 0;
     var m = true;
     //连续动画需要添加定时器,所传参数每次+1就行
     setInterval(function () {
       n = n + 1;
-      console.log(n);
+      k = k + 1;
+      console.log(k);
       if (m) {
         this.setData({
-          imgeUrlAni: "../imgs/gif1/{{n}}.jpg",
+          imgeUrlAni: "../imgs/gif1/" + n + ".jpg",
+          imgeUrlAni1: "../imgs/gif2/" + k + ".png",
         })
-        // this.animation.scale(1.2, 1.2).step()
+        if (k > 6) {
+          k = 0
+        }
+        if (n > 12) {
+          n = 0
+        }
         m = !m;
       } else {
-        imgeUrlAni: "../imgs/gif1/{{n}}.jpg",
-        // this.animation.scale(1, 1).step()
         m = !m;
       }
-      
-
       this.setData({
         animationData: this.animation.export()
       })
-    }.bind(this), 2000)
+    }.bind(this), 100)
+  }
+  ,
+  
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
 
-
-
+    
     var that = this;
+    that.animationFunc()
     
     setTimeout(function () {
       app.show(that, 'slide1', 1)
-     
+
     }.bind(this), 2000);
-    
+
     wx.getSystemInfo({
       success: function (res) {
 
         console.log(res.windowHeight);
         setTimeout(function () {
-          app.slideupshow(that, 'slide_up1', -res.windowHeight/2.6, 1)
-          
+          app.slideupshow(that, 'slide_up1', -res.windowHeight / 2.6, 1)
+
         }.bind(that), 200);
         setTimeout(function () {
           app.slideupshow(that, 'slide_up2', -res.windowHeight / 2.6, 1)
@@ -279,32 +290,32 @@ Page({
         }.bind(that), 600);
       },
     })
-  
-  //   
-  //   GetList(that);
 
-  //   setTimeout(function() {
-  //     //要延时执行的代码
-  //     that.startDown();
-  //   }, 1000) //延迟)
-  //   setTimeout(function() {
-  //     //要延时执行的代码
-  //     that.startDown1();
-  //   }, 1500) //延迟)
-  // },
-  // startDown: function(e) {
-  //   this.setData({
-  //     isDown: true,
-  //     percent: 2,
-  //   })
-  // },
-  // startDown1: function(e) {
-  //   this.setData({
-  //     isDown1: true,
-  //     percent1: 50,
-  //   })
+    //   
+    //   GetList(that);
+
+    //   setTimeout(function() {
+    //     //要延时执行的代码
+    //     that.startDown();
+    //   }, 1000) //延迟)
+    //   setTimeout(function() {
+    //     //要延时执行的代码
+    //     that.startDown1();
+    //   }, 1500) //延迟)
+    // },
+    // startDown: function(e) {
+    //   this.setData({
+    //     isDown: true,
+    //     percent: 2,
+    //   })
+    // },
+    // startDown1: function(e) {
+    //   this.setData({
+    //     isDown1: true,
+    //     percent1: 50,
+    //   })
   },
-  showview: function() {
+  showview: function () {
 
     var that = this;
     this.setData({
@@ -314,13 +325,13 @@ Page({
     })
 
   },
-  hideview: function() {
+  hideview: function () {
     this.setData({
       display: "none",
 
     })
   },
-  onReady: function() {
+  onReady: function () {
 
     // var context = wx.createContext();//创建并返回绘图上下文context对象。
     // context.beginPath();//开始一个新的路径
@@ -339,14 +350,14 @@ Page({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
     var that = this;
 
   },
@@ -354,26 +365,26 @@ Page({
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function(res) {
+  onShareAppMessage: function (res) {
     return {
       title: '慧吃慧动100天',
       // 分享时在路径后拼接参数，可拼接多个参数。 
       path: '/pages/home/home?id=13624249960',
-      success: function(res) {
+      success: function (res) {
         // 转发成功
         console.log("转发成功")
         wx.showToast({
@@ -382,43 +393,43 @@ Page({
           duration: 2000,
         })
       },
-      fail: function(res) { // 转发失败
+      fail: function (res) { // 转发失败
         console.log("转发失败")
       }
     }
   },
-  ToRankList: function(e) {
+  ToRankList: function (e) {
     console.log("id == " + e.currentTarget.id)
     wx.navigateTo({
       url: '../rankList/rankList?id=' + e.currentTarget.id
     })
   },
-  ToRulesView: function(e) {
+  ToRulesView: function (e) {
 
 
 
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
 
 
-      app.globalData.userInfo = e.detail.userInfo
-      this.setData({
-        userInfo: e.detail.userInfo,
-        hasUserInfo: true
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
 
-      })
+    })
 
-    }
+  }
 
-    ,
-  toReport: function(e) {
+  ,
+  toReport: function (e) {
 
     console.log(e.target.id)
     wx.navigateTo({
       url: '../report/report'
     })
   },
-  show: function() {
+  show: function () {
 
     this.setData({
       flag: false
@@ -427,7 +438,7 @@ Page({
   },
   //消失
 
-  hide: function() {
+  hide: function () {
 
     this.setData({
       flag: true
