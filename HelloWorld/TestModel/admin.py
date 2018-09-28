5964555701e5c7694bc56413e5582dc42e541879
zhaobin <4480779023@qq.com>
2018-09-28 13:34:03 +0800

    小程序 遮罩层

--- HelloWorld/TestModel/admin.py
+++ HelloWorld/TestModel/admin.py
@@ -5,6 +5,22 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from TestModel.models import Test
+from TestModel.models import Test,Contact,Tag
 # Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'email')  # list
+    fieldsets = (
+        ['Main', {
+            'fields': ('name', 'email'),
+        }],
+        ['Advance', {
+            'classes': ('collapse',),
+            'fields': ('age',),
+        }]
+
+    )
+
+
+admin.site.register(Contact, ContactAdmin)
 admin.site.register([Test])
